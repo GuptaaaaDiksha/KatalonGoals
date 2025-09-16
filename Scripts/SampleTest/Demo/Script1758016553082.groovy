554a7146157b1f07167979171d54778771556dcf
Diksha Gupta <Diksha Gupta@DESKTOP-OHD8FDL>
2025-09-16 20:39:45 +0530

    Inconsistent merge state: could not find file MERGE_MSG in .git folder. This file contains the commit message for a merge commit.

--- Scripts/SampleTest/Demo/Script1758016553082.groovy
+++ Scripts/SampleTest/Demo/Script1758016553082.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.openBrowser('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
 WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 //enter user name
