c79917103b50ed5dcefbfa63cace9deeb91e3fc4
Diksha Gupta <Diksha Gupta@DESKTOP-OHD8FDL>
2025-09-16 18:42:44 +0530

    Added demo suite

---
+++ Test Suites/DemoSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fee7728a-bde9-41c8-b5e6-723211bd87cb</testSuiteGuid>
+   <testCaseLink>
+      <guid>2b9ff0d2-7d77-4d05-9c79-3cc059932ef5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SampleTest/Demo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
